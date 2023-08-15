285ab6c2c6ce129a46a4cdbcc59d47a8093c580f
Tabitha_Townes <46570035+TMTownes@users.noreply.github.com>
2023-08-14 19:48:40 -0700

    change Username min

--- index.js
+++ index.js
@@ -284,7 +284,7 @@ app.get('/movies/director/:Name', passport.authenticate('jwt', {session: false})
     Birthday: Date
 } */
 app.post('/users', [
-    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username is required').isLength({min: 4}),
     check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
     check('Password', 'Password is required').not().isEmpty(),
     check('Email', 'Email does not appear to be valid').isEmail()
