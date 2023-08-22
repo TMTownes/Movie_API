ac3d7c211796c305c27ea2477f70d85e15003552
Tabitha_Townes <46570035+TMTownes@users.noreply.github.com>
2023-08-21 21:12:58 -0700

    Update cors to allow user sign in

--- index.js
+++ index.js
@@ -43,15 +43,15 @@ const cors = require('cors');
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'https://myflix-retro-af49f4e11172.herokuapp.com'];
 
 app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-            //if specific origin not found on allowed list
-            let message = 'The CORS policy for this application does not allow access from origin' + origin;
-            return callback(new Error(message), false);
-        }
-        return callback(null, true);
-    }
+    // origin: (origin, callback) => {
+    //     if (!origin) return callback(null, true);
+    //     if(allowedOrigins.indexOf(origin) === -1){
+    //         //if specific origin not found on allowed list
+    //         let message = 'The CORS policy for this application does not allow access from origin' + origin;
+    //         return callback(new Error(message), false);
+    //     }
+    //     return callback(null, true);
+    // }
 }));
 
 
